751eaf78088f44768268baa49da18ff6353f8194
Солнце <@LAPTOP-DPKNJGCI>
2023-02-13 19:55:57 +0300

    Добавьте файлы проекта.

---
+++ CM_11/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_11
+{
+    struct Line
+    {
+        double k;
+        double b;
+    
+     public Line(double k, double b)
+      {
+         this.k = k;
+         this.b = b;
+       }
+    public string Root()
+       {
+        if ((k == 0) && (b == 0))
+            return "Решением уравнения является любое число";
+        else if ((k == 0) && (b != 0))
+            return "Уравнение решений не имеет";
+        double x = (b / k);
+            return $"Решение уравнения - {x}";
+       }
+    }
+}
+
---
+++ CM_11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+           
+            Line line = new Line(k,b);
+            Console.WriteLine(line.Root());
+            Console.ReadKey();
+        }
+    }
+}
